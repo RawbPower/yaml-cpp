44569bb8a8945345db8882a14efd353e691c7d51
RawbPower <RawbPower@users.noreply.github.com>
2023-12-02 22:54:23 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -9,7 +9,7 @@ project "yaml-cpp"
 	{
 		"src/**.h",
 		"src/**.cpp",
-		
+
 		"include/**.h"
 	}
 
@@ -18,16 +18,21 @@ project "yaml-cpp"
 		"include"
 	}
 
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
+
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
+		staticruntime "off"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
+		staticruntime "off"
 
 	filter "configurations:Debug"
 		runtime "Debug"
